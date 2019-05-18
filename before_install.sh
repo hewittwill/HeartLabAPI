b81ceefea60e81be885e99f949e562abdac5bd15
Will Hewitt <me@hewittwill.com>
2019-05-18 14:29:27 +1200

    Summarised

--- before_install.sh
+++ before_install.sh
@@ -2,4 +2,10 @@ sudo yum update
 
 sudo yum install -y \
     httpd \
-    mod_wsgi
+    apache2-dev \
+    mod_wsgi 
+
+if [ -d /home/heartlab-api ]; then
+  sudo rm -R /home/heartlab-api
+  mkdir /home/heartlab-api
+fi
